ef1f02e3607fb329387de9a789a966076c5ee8e6
boripat874 <boripat874@gmail.com>
2026-02-19 15:25:15 +0700

    ver ver 1.2.0 190226

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       args: "", // อาร์กิวเมนต์ที่ส่งไปยัง script (ในที่นี้คือ 'start' เพื่อรัน Production build ของ Next.js)
       cwd: "./", // working directory ของแอปพลิเคชัน
       env: {
-        PORT: 3001, // กำหนด environment variable PORT เป็น 8080
+        PORT: 443, // กำหนด environment variable PORT เป็น 8080
         NODE_ENV: "production", // กำหนด environment เป็น production
       },
       // (ตัวเลือกอื่นๆ เช่น instances, autorestart, watch, logs)
