c966eaedd6e6b14ce78df5ca65b1a61e425e1ff0
boripat874 <boripat874@gmail.com>
2026-02-19 18:38:46 +0700

    ver 1.0.1 190226

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -2,12 +2,12 @@ export const config = {
 
     // apiUrl: 'http://35.186.144.224:80/api',
 
-    apiUrl: 'https://10.10.46.102:443/api',
-
+    apiUrl: 'https://10.10.46.2:443/api',
+// 
     // apiUrlImage: 'http://35.186.144.224:80',
 
-    apiUrlImage: 'https://10.10.46.102:443',
-
+    apiUrlImage: 'https://10.10.46.2:443',
+// 
     apiKey: '494eabae-b0f7-4d0d-a436-5af39c3abf62',
     // apiUrlZoo: 'https://devsmartzoo-api.netbay.co.th/external/pos',
     apiUrlZoo: 'https://uatsmartzoo-api.netbay.co.th/external/pos',
