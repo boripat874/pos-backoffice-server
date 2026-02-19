6e009fae48160170a2a1d893bea013ba9df7182d
boripat874 <boripat874@gmail.com>
2026-02-19 15:53:07 +0700

    ver ver 1.2.0 190226

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       args: "start", // อาร์กิวเมนต์ที่ส่งไปยัง script (ในที่นี้คือ 'start' เพื่อรัน Production build ของ Next.js)
       // cwd: "./", // working directory ของแอปพลิเคชัน
       env: {
-        PORT: 443, // กำหนด environment variable PORT เป็น 8080
+        PORT: 3001, // กำหนด environment variable PORT เป็น 8080
         NODE_ENV: "production", // กำหนด environment เป็น production
         NODE_TLS_REJECT_UNAUTHORIZED: "0", // แก้ปัญหาดึงรูปไม่ได้จาก SSL (Not Secure)
       },
