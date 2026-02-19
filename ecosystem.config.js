5902e1c2860b987df5a014a599415e272053ca6e
boripat874 <boripat874@gmail.com>
2026-02-19 15:28:03 +0700

    ver ver 1.2.0 190226

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "POS-backofficeWeb", // ชื่อแอปพลิเคชันที่คุณต้องการให้ PM2 จัดการ
-      script: "./server.js", // คำสั่งที่จะรัน (สามารถเป็น 'npm' หรือ 'yarn')
+      script: "npm", // คำสั่งที่จะรัน (สามารถเป็น 'npm' หรือ 'yarn')
       watch: true,
       instances  : 1,
       args: "", // อาร์กิวเมนต์ที่ส่งไปยัง script (ในที่นี้คือ 'start' เพื่อรัน Production build ของ Next.js)
@@ -10,6 +10,7 @@ module.exports = {
       env: {
         PORT: 443, // กำหนด environment variable PORT เป็น 8080
         NODE_ENV: "production", // กำหนด environment เป็น production
+        NODE_TLS_REJECT_UNAUTHORIZED: "0", // แก้ปัญหาดึงรูปไม่ได้จาก SSL (Not Secure)
       },
       // (ตัวเลือกอื่นๆ เช่น instances, autorestart, watch, logs)
     },
